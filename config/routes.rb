9be009d09b7deff5a20ff7b68a6222c6783590a0
portal martin <ptl_martin@yahoo.co.jp>
2024-01-09 22:10:58 +0900

    reviews_controller = ReviewsController.new restaurants_controller = RestaurantsController.new

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # get 'reviews/show'
-  # get 'restaurants/index'
-  # get 'restaurants/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :restaurants do
     resources :reviews, only: [:new, :create] # nested resource
   end
